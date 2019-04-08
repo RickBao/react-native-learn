570daf3f96299f2604c315aae4c287709cb94969
baojianfei <baojianfei@cocheer.net>
2019-04-08 20:38:12 +0800

    实现通过fetch获取url中的数据,并保存在FlatList中,展示出来

---
+++ App.js
@@ -0,0 +1,181 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {AppRegistry, Platform,TextInput, StyleSheet, Text, View, Image} from 'react-native';
+
+/**
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+*/
+/**
+export default class HelloWorldApp extends Component {
+ render() {
+	return (
+		<Text>Hello World!</Text>
+	);
+	}
+}
+*/
+/**
+export default class Bananas extends Component{
+	render() {
+		let pic = { uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'};
+		return ( <Image source={pic} style={{width: 193, height: 110}} /> );
+	}
+}
+*/
+/**
+class Greeting extends Component {
+	render() { return ( <Text> Hello {this.props.name}!</Text>);}
+}
+
+export default class LotsOfGreetings extends Component {
+	render() { return ( <View style={{alignItems: 'center'}}>
+				<Greeting name='RxJava' />
+				<Greeting name='Snow' />
+				<Greeting name='Love' />
+			</View> ); }
+}	
+*/
+
+/**
+class Blink extends Component {
+	constructor(props) { 
+		super(props);
+		this.state = { showText: true};
+		setInterval(() => { 
+			this.setState(previousState => {
+				return { showText: !previousState.showText };
+			});
+		}, 1000);
+	}
+	render() {
+		let display = this.state.showText ? this.props.text: ' ';
+		return ( <Text>{display}</Text>);
+	}
+}
+
+export default class BlinkApp extends Component {
+	render() { return ( <View>
+				<Blink text='I love the blink' />
+				<Blink text='Yes blinking is so great' />
+				<Blink text='Why did they ever take this out of HTML' />
+				<Blink text='Look at me Love Snow' />
+			</View> );}
+}
+*/
+
+/**
+export default class LotsOfStyles extends Component {
+	render() { return ( <View>
+				<Text style={styles.red}>just red</Text>
+				<Text style={styles.bigblue}>just bigblue</Text>
+				<Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+				<Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+			</View>);}
+	}
+
+const styles = StyleSheet.create({
+	bigblue: {
+		color:'blue',
+		fontWeight:'bold',
+		fontSize:30,
+	},
+	red: {
+		color:'red',
+	},
+});
+AppRegistry.registerComponent('LotsOfStyles', ()=> LotsOfStyles);
+*/
+
+/**
+export default class FixedDimensionsBasics extends Component {
+	render() {
+		return ( <View style={{flex:1, flexDirection:'column-reverse'}}>
+			<View style={{flex:1, backgroundColor:'powderblue'}} />
+			<View style={{flex:2, backgroundColor:'skyblue'}} />
+			<View style={{flex:3, backgroundColor:'steelblue'}} />
+			</View>
+		);
+	}
+};
+
+AppRegistry.registerComponent('AwesomeProject',()=> FixedDimensionsBasics);	
+
+*/
+
+/**
+
+export default class JustifyContentBasics extends Component {
+	render() {
+		return (
+			<View style={{ flex:1, flexDirection:'column',justifyContent:'center', alignItems:'center',}}>
+				<View style={{width:50, height:50, backgroundColor:'powderblue'}} />
+				<View style={{width:50, height:50, backgroundColor:'skyblue'}} />
+				<View style={{width:50, height:50, backgroundColor:'steelblue'}} />
+			</View>
+		);
+	}
+};
+
+AppRegistry.registerComponent('AwesomeProject',()=> JustifyContentBasics);
+*/
+
+export default class PizzaTranslator extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {text: ' '};
+	}
+	render() {
+		return ( <View style={{padding:10}}>
+				<TextInput style={{height:40}}
+					placeholder="Type here to translate!"
+					onChangeText={(text) => this.setState({text})} />
+			<Text style={{padding:10, fontSize:42}}>
+				{this.state.text.split(' ').map((word) => word && 'o').join(' ')}
+			</Text>
+		</View>
+	);
+}}
